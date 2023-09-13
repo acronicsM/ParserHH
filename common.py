71255dd95350f799cceb5fede921eabb3f8071a5
param <iam@iam.com>
2023-09-13 19:46:25 +0300

    Доделал парсер валют, начал анализ данных

--- common.py
+++ common.py
@@ -26,7 +26,7 @@ class Vacancy:
                  'currency',
                  )
 
-    def __init__(self, vac_id: int, name: str, raw_json: str = None):
+    def __init__(self, vac_id: int, name: str, raw_json: str = None, courses: dict | None = None):
         self.vac_id, self.name = vac_id, name
         self.salary_from = self.salary_to = 0
         self.type = self.requirement = self.responsibility = self.experience = self.employment = self.description = ''
@@ -37,9 +37,9 @@ class Vacancy:
         self.relevance_date = datetime.now()
 
         if raw_json:
-            self.__parser_raw_json(raw_json)
+            self.__parser_raw_json(raw_json, courses)
 
-    def __parser_raw_json(self, raw_json):
+    def __parser_raw_json(self, raw_json, courses):
         published_at = raw_json['published_at']
         published_at = published_at[:published_at.find('+')]
 
@@ -49,10 +49,23 @@ class Vacancy:
         self.responsibility = raw_json['snippet']['responsibility']
         self.experience = raw_json['experience']['name']
         self.employment = raw_json['employment']['name']
-        self.currency = raw_json['salary']['currency']
 
-        self.salary_from = raw_json['salary']['from'] if raw_json['salary'] else None
-        self.salary_to = raw_json['salary']['to'] if raw_json['salary'] else None
+        if raw_json['salary']:
+            self.currency = raw_json['salary']['currency']
+            self.salary_from = raw_json['salary']['from'] if raw_json['salary']['from'] else 0
+            self.salary_to = raw_json['salary']['to'] if raw_json['salary']['to'] else 0
+        else:
+            self.currency = 'RUB'
+            self.salary_from = 0
+            self.salary_to = 0
+
+        if courses:
+            course = courses[self.currency]
+            if not course:
+                print(f'Не удалось конвертировать валюту {self.currency}')
+            else:
+                self.salary_from = self.salary_from * course
+                self.salary_to = self.salary_to * course
 
     def save(self):
         exists_and_makedir(VACANCY_FOLDER)
@@ -96,17 +109,16 @@ def get_vacancy_for_update_bin():
     return vacancies_for_update
 
 
-def save_vacancy_from_json(data_json: dict):
+def save_vacancy_from_json(data_json: dict, courses: dict | None = None) -> None:
     for v in data_json['items']:
         vac_id = v['id']
         vacancy = None
         for path in Path(VACANCY_FOLDER).glob(f'{vac_id}.bin'):
             vacancy = get_vacancy_obj(path)
             vacancy.relevance_date = datetime.now()
-            vacancy.need_update = False
 
         if vacancy is None:
-            vacancy = Vacancy(vac_id=vac_id, name=v['name'], raw_json=v)
+            vacancy = Vacancy(vac_id=vac_id, name=v['name'], raw_json=v, courses=courses)
 
         vacancy.save()
 
@@ -117,7 +129,7 @@ def update_detail_vacancy(vacancy: Vacancy, detail_data: dict):
     vacancy.key_skills = detail_data['key_skills']
     vacancy.description_skills = detail_data['description_skills']
     vacancy.basic_skills = detail_data['basic_skills']
-    vacancy.basic_skills = detail_data['need_update']
+    vacancy.need_update = False
 
 
 def get_all_vacancies():
--- data_analysis.py
+++ data_analysis.py
@@ -1,46 +1,50 @@
 from common import get_all_vacancies, Vacancy
+import seaborn as sns
 
 
-def collect_data_skills(skills_dict: dict, skill: str, vacancy: Vacancy):
+
+def create_collect_data_skills(skills_dict: dict, skill: str, vacancy: Vacancy):
     if skill not in skills_dict:
         skills_dict[skill] = {'count_vacancy': 0, 'salary': set()}
 
-    if vacancy.salary_from is not None:
-        skills_dict[skill]['salary'].add(vacancy.salary_from)
-
-    if vacancy.salary_to is not None:
-        skills_dict[skill]['salary'].add(vacancy.salary_to)
-
-    if vacancy.salary_from is not None and vacancy.salary_to is not None:
-        skills_dict[skill]['salary'].add((vacancy.salary_from + vacancy.salary_to) / 2)
+    skills_dict[skill]['salary'].add(vacancy.salary_from)
+    skills_dict[skill]['salary'].add(vacancy.salary_to)
+    skills_dict[skill]['salary'].add((vacancy.salary_from + vacancy.salary_to) / 2)
 
     skills_dict[skill]['count_vacancy'] += 1
 
 
+def print_collect_data_skills(skills_dict: dict):
+    for skill, v in skills_dict.items():
+        min_salary, max_salary = min(v['salary']), max(v['salary'])
+        average_salary = (min_salary + max_salary) / 2
+        print(f'{skill}: {min_salary:_} {average_salary:_} {max_salary:_}')
+
+
+def collect_data_skills(vacs: list[Vacancy]):
+    key = dict()
+    description = dict()
+    basic = dict()
+
+    for vacancy in vacs:
+        for skill in vacancy.key_skills:
+            create_collect_data_skills(key, skill, vacancy)
+
+        for skill in vacancy.description_skills:
+            create_collect_data_skills(description, skill, vacancy)
+
+        for skill in vacancy.basic_skills:
+            create_collect_data_skills(basic, skill, vacancy)
+
+    return key, description, basic
+
+
 if __name__ == '__main__':
 
-    # vacancies = get_all_vacancies()
-    #
-    # key_skills = description_skills = basic_skills = dict()
-    #
-    # for vacancy in vacancies:
-    #     for skill in vacancy.key_skills:
-    #         collect_data_skills(key_skills, skill, vacancy)
-    #
-    #     for skill in vacancy.description_skills:
-    #         collect_data_skills(description_skills, skill, vacancy)
-    #
-    #     # for skill in vacancy.basic_skills:
-    #     #     collect_data_skills(basic_skills, skill, vacancy)
-    #
-    # print(f'Всего вакансий: {len(vacancies)}')
-    #
-    # for skill, v in key_skills.items():
-    #     min_salary, max_salary = min(v['salary']), max(v['salary'])
-    #     average_salary = (min_salary + max_salary) / 2
-    #     print(f'{skill}: {min_salary:_} {average_salary:_} {max_salary:_}')
-    # # print('key_skills', key_skills, '', sep='\n')
-    # # print('description_skills', description_skills, '', sep='\n')
-    # # print('basic_skills', basic_skills, '', sep='\n')
+    vacancies = get_all_vacancies()
 
+    key_skills, description_skills, basic_skills = collect_data_skills(vacancies)
 
+    print(f'Всего вакансий: {len(vacancies)}')
+
+    print_collect_data_skills(basic_skills)
